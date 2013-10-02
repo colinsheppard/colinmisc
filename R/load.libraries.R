189951b467696042b6543eda3fc55dbb677e4cd2
colinsheppard <colinthegaunt@gmail.com>
2013-10-02 10:25:54 -0700

    allow quietly option during library load

--- R/load.libraries.R
+++ R/load.libraries.R
@@ -1,11 +1,11 @@
 load.libraries <-
-function(needed.packs){
+function(needed.packs,quietly=F){
   installed.packs <- installed.packages()
 
   for(pack in needed.packs){
     if(length(grep(pack,installed.packs))==0){
       install.packages(pack,repos='http://cran.cnr.Berkeley.edu')
     }
-    library(pack,character.only=T)
+    library(pack,character.only=T,quietly=quietly)
   }
 }
