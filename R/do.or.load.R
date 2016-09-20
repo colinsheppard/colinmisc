16217abbe1c096e30b4588fa69b270def572991e
Colin Sheppard <colinthegaunt@gmail.com>
2016-09-20 16:24:01 -0700

    getting over bug when environment is locked

--- R/do.or.load.R
+++ R/do.or.load.R
@@ -1,9 +1,11 @@
-do.or.load <- function(file.path,fun,...){
+do.or.load <- function(file.path,fun,args.to.save=NA,...){
+  if(!is.na(args.to.save)){
+    suppressWarnings(streval(pp('rm("',pp(args.to.save,collapse='","'),'",inherits=T)')))
+  }
   if(!file.exists(file.path)){
     # do function call here so that vars are added to the frame of this function call....
     res <- fun(...)
     my.cat(res)
-    suppressWarnings(streval(pp('rm("',pp(names(res),collapse='","'),'",inherits=T)')))
     attach(res,warn.conflicts=F)
     streval(pp('save(',pp(names(res),collapse=','),',file="',file.path,'")'))
     my.cat(pp('Variables: ',pp(names(res),collapse=','),' created and saved to ',file.path))
