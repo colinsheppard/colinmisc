b77e56c18775d414355736af54aaa855bb818e5f
Colin Sheppard <colinthegaunt@gmail.com>
2016-09-20 14:06:42 -0700

    fixing issue where existing instances of variables from paranet environment do not get overwritten

--- R/do.or.load.R
+++ R/do.or.load.R
@@ -2,6 +2,8 @@ do.or.load <- function(file.path,fun,...){
   if(!file.exists(file.path)){
     # do function call here so that vars are added to the frame of this function call....
     res <- fun(...)
+    my.cat(res)
+    suppressWarnings(streval(pp('rm("',pp(names(res),collapse='","'),'",inherits=T)')))
     attach(res,warn.conflicts=F)
     streval(pp('save(',pp(names(res),collapse=','),',file="',file.path,'")'))
     my.cat(pp('Variables: ',pp(names(res),collapse=','),' created and saved to ',file.path))
@@ -9,6 +11,6 @@ do.or.load <- function(file.path,fun,...){
   }else{
     my.cat(pp('Loading: ',file.path))
   }
-  load(file.path,parent.frame(2),verbose = T)
+  load(file.path,envir=parent.frame(2),verbose = T)
 }
 
